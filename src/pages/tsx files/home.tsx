33355d66317a37365e8da010cf40c9a734e25813
*InsertUsernameHere* <rohitvankamamidi864@gmail.com>
2025-07-11 09:21:32 -0400

    Added a work in progress homepage

--- src/pages/tsx files/home.tsx
+++ src/pages/tsx files/home.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import '../css/home.css';
-import ShinyText from '../../components/TextAnimations/ShinyText/ShinyText';
+import DecryptedText from '../../../public/TextAnimations/DecryptedText/DecryptedText.tsx';
 
 function Home() {
+  const name = "Robothletes";
+
   return (
     <div className="home bg-black">
-      <div id='home-header' className='text-center'>
-        <ShinyText text="Just some shiny text!" disabled={false} speed={3} className='custom-class' />
-        <h3 className="text-white">FTC Team #24611</h3>
+      <div id="home-header" className="text-center font-bold">
+        {name.split('').map((char, index) => (
+          <DecryptedText
+            key={index}
+            text={char} //Imma still try this out
+            speed={(200 / name.length) * index} 
+            maxIterations={20}
+            characters="ABC123!?"
+            className="revealed title"
+            parentClassName="all-letters"
+            encryptedClassName="encrypted title"
+            animateOn="view"
+          />
+        ))}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shine: {
+          '0%': { 'background-position': '100%' },
+          '100%': { 'background-position': '-100%' },
+        },
+      },
+      animation: {
+        shine: 'shine 5s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
